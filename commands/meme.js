5bd1f9aab5a2a5d0904866958113c5e463577794
Farris Bris <farrisbris@protonmail.com>
2018-12-23 17:45:59 +0100

    Meme push

--- commands/meme.js
+++ commands/meme.js
@@ -1,9 +1,15 @@
-exports.use = async (client, message, args, command, discord) => {
+const got = require("got");
 
+exports.use = async (client, message, args, command, discord) => {
+    got('https://www.reddit.com/r/meme/random/.json').then(response => {
+        let content = JSON.parse(response.body);
+        var image = content[0].data.children[0].data.url;
+        message.channel.send(image)
+    })
 }
 
 exports.help = {
-    name: "Base",
+    name: "meme",
     description: "Base",
     category: "Base",
     aliases: []
