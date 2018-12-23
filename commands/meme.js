4ed83f121a2e8e7ff8ca9efde50911b1ba7e47c8
Farris Bris <farrisbris@protonmail.com>
2018-12-23 22:05:08 +0100

    Changed reddit meme provider

--- commands/meme.js
+++ commands/meme.js
@@ -1,11 +1,33 @@
 const got = require("got");
 
 exports.use = async (client, message, args, command, discord) => {
-    got('https://www.reddit.com/r/meme/random/.json').then(response => {
-        let content = JSON.parse(response.body);
-        var image = content[0].data.children[0].data.url;
-        message.channel.send(image)
-    })
+    getMeme()
+
+    function getMeme() {
+        got('https://www.reddit.com/r/memes/random/.json').then(response => {
+            let content = JSON.parse(response.body);
+            if (!content[0]) {
+                getMeme()
+            }
+            var image = content[0].data.children[0].data.url;
+            if (!image) {
+                getMeme()
+            }
+            var title = content[0].data.children[0].data.title;
+            if (!title) {
+                getMeme()
+            }
+
+            let embed = new discord.RichEmbed()
+                .setColor()
+                .setTitle("Meme requested by " + message.author.tag)
+                .setDescription(title)
+                .setImage(image)
+                .setFooter("Bot made by Farris Bris#7705")
+            message.channel.send(embed)
+        }).catch()
+    }
+
 }
 
 exports.help = {
